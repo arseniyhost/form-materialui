289a197a759b29675a42d9f298762aeb470e70cc
arseniyhost <pilipenko_arseniy@mail.ru>
2019-11-20 17:27:59 +0200

    test

---
+++ test/precache-manifest.d505259835f0ec66b5d83897e7b2ba52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc445607e72f816b6ffd5e7576715264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304f4db90e77c1bc890f",
+    "url": "/static/css/main.385563d1.chunk.css"
+  },
+  {
+    "revision": "432158f1034cb5f7c66c",
+    "url": "/static/js/2.06e03783.chunk.js"
+  },
+  {
+    "revision": "304f4db90e77c1bc890f",
+    "url": "/static/js/main.9bc0caae.chunk.js"
+  },
+  {
+    "revision": "bb343a407ba298bba0e7",
+    "url": "/static/js/runtime-main.b96307ab.js"
+  }
+]);
