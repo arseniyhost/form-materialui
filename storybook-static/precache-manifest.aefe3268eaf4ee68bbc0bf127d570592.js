d88d4a70244409ac273ebb380577554e498ef007
arseniyhost <pilipenko_arseniy@mail.ru>
2019-11-21 08:27:42 +0200

    test-task-by-storybook

---
+++ storybook-static/precache-manifest.aefe3268eaf4ee68bbc0bf127d570592.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91df3ae805b05452651c763f850ffe16",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6d1453bb712061ad4cf1.bundle.js"
+  },
+  {
+    "revision": "75520036257838b30717",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.6d1453bb712061ad4cf1.bundle.js"
+  },
+  {
+    "url": "vendors~main.6d1453bb712061ad4cf1.bundle.js"
+  }
+]);
