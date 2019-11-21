67d70a47f783ecefb7fffd8e017bd377ceb0271e
arseniyhost <pilipenko_arseniy@mail.ru>
2019-11-21 08:20:16 +0200

    test-task

---
+++ test/precache-manifest.ed3490f1f6262d2452133f6f79c7288a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd38c9be9fc64dcd7852f4f2bdb4fef",
+    "url": "/form-materialui/test/index.html"
+  },
+  {
+    "revision": "2deed5ab06c989107599",
+    "url": "/form-materialui/test/static/css/main.79cd6f02.chunk.css"
+  },
+  {
+    "revision": "c0625f4f9bbccaf5965a",
+    "url": "/form-materialui/test/static/js/2.58e8edb6.chunk.js"
+  },
+  {
+    "revision": "2deed5ab06c989107599",
+    "url": "/form-materialui/test/static/js/main.2c70f142.chunk.js"
+  },
+  {
+    "revision": "17026072f09ab1d51fcd",
+    "url": "/form-materialui/test/static/js/runtime-main.97aed45e.js"
+  }
+]);
--- test/service-worker.js
+++ test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d505259835f0ec66b5d83897e7b2ba52.js"
+  "/form-materialui/test/precache-manifest.ed3490f1f6262d2452133f6f79c7288a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/form-materialui/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
